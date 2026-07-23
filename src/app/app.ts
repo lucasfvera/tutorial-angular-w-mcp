7e05bdbe4ac91b83fed653a4d55cd876f1c4806d
lucasfvera <lucasferreyravera@gmail.com>
2026-07-22 22:41:04 -0300

    Phase 2, Module 4

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MOCK_RECIPES } from './mock-recipes';
+import { RecipeModel } from './models';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +10,13 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('My Recipe Box');
+  protected readonly title = signal<string>('My Recipe Box');
+  protected readonly recipe = signal<RecipeModel>(MOCK_RECIPES[0])
 
-  protected handleRecipeClick(): void {
-    console.log("Recipe clicked!")
+  protected handleRecipeClick(recipeId: number): void {
+    console.log("Recipe clicked!", recipeId)
+    const selectedRecipe = MOCK_RECIPES.find(r => r.id === recipeId)
+    if (!selectedRecipe) return;
+    this.recipe.set(selectedRecipe)
   }
 }
