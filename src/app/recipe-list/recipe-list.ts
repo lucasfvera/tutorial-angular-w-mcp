361ebf0e47206e4ba87b878bb1de01f6bed6c0ab
lucasfvera <lucasferreyravera@gmail.com>
2026-07-28 14:11:32 -0300

    Phase 4, Module 13

--- src/app/recipe-list/recipe-list.ts
+++ src/app/recipe-list/recipe-list.ts
@@ -1,18 +1,22 @@
 import { Component, computed, signal } from '@angular/core';
 import { RecipeModel } from '../models';
 import { MOCK_RECIPES } from '../mock-recipes';
-import { JsonPipe } from '@angular/common';
 import { RecipeDetail } from "../recipe-detail/recipe-detail";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-list',
-  imports: [JsonPipe, RecipeDetail],
+  imports: [RecipeDetail, FormsModule],
   templateUrl: './recipe-list.html',
   styleUrl: './recipe-list.css',
 })
 export class RecipeList {
   protected readonly title = signal<string>('My Recipe Box');
+  protected readonly allRecipes = signal<RecipeModel[]>(MOCK_RECIPES)
   protected readonly recipe = signal<RecipeModel>(MOCK_RECIPES[0])
+  protected readonly searchTerm = signal<string>("")
+
+  protected readonly filteredRecipes = computed<RecipeModel[]>(() => this.allRecipes().filter((r) => r.name.toLocaleLowerCase().includes(this.searchTerm().toLocaleLowerCase())))
 
   protected handleRecipeClick(recipeId: number): void {
     console.log("Recipe clicked!", recipeId)
